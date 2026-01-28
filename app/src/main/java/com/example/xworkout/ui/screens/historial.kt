ee0e2df596e1b159463989356e80dcece88421ca
juan-mezcla <juanmezcua96@gmail.com>
2026-01-28 23:02:01 +0100

    vistas home, historial y perfil terminadas, generadorRutina falta

--- app/src/main/java/com/example/xworkout/ui/screens/historial.kt
+++ app/src/main/java/com/example/xworkout/ui/screens/historial.kt
@@ -1,21 +1,113 @@
 package com.example.xworkout.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview(showBackground = true)
 @Composable
 fun historialView(){
     Box(
         modifier = Modifier.fillMaxSize()
+            .background(Color.DarkGray)
     ){
-        Text(text = "historial", modifier = Modifier.padding(16.dp))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+            ,modifier = Modifier.fillMaxSize()
+                .padding(15.dp).height(90.dp)) {
+            Spacer(modifier = Modifier.padding(10.dp))
+            Text(text = "Historial de rutinas", color = Color.Red,
+                fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                fontSize = 25.sp)
+
+            Spacer(modifier = Modifier.padding(5.dp))
+
+            Text(text = "Desliza para eliminar", color = Color.LightGray,
+                fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                fontSize = 15.sp)
+
+            Spacer(modifier = Modifier.padding(20.dp))
+
+            Row(Modifier.fillMaxWidth()) {
+                LazyColumn(modifier = Modifier.wrapContentHeight()
+                    .fillMaxWidth()) {
+
+
+                    item {
+                        // Quitamos la Column intermedia que no aportaba nada
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                // 1. EL FONDO: Debe tener el mismo shape que el borde para no salirse de las esquinas
+                                .background(Color(0xFF2B2B2B), shape = RoundedCornerShape(10.dp))
+                                // 2. EL BORDE
+                                .border(2.dp, Color.Red, shape = RoundedCornerShape(10.dp))
+                                .wrapContentHeight()
+                                .padding(25.dp) // El padding va AL FINAL para que sea espacio interno
+                        ) {
+                            Column {
+                                Text(
+                                    text = "Fecha",
+                                    color = Color.White,
+                                    fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                                    fontSize = 18.sp
+                                )
+                                Text(
+                                    text = "Tipo rutina",
+                                    fontWeight = androidx.compose.ui.text.font.FontWeight.Bold,
+                                    color = Color.White,
+                                    fontSize = 22.sp
+                                )
+                                Text(
+                                    text = "tiempo · calorias · dificultad",
+                                    fontWeight = androidx.compose.ui.text.font.FontWeight.Normal,
+                                    color = Color.LightGray,
+                                    fontSize = 14.sp
+                                )
+                            }
+
+                            Image(
+                                painter = painterResource(com.example.xworkout.R.drawable.logo_x_workout),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .size(35.dp)
+                                    .clip(CircleShape)
+                                    .border(1.dp, Color.Red, shape = CircleShape)
+                            )
+                        }
+
+                    }
+                }
+            }
+        }
+
+
 
     }
 }
--- app/src/main/java/com/example/xworkout/ui/screens/perfil.kt
+++ app/src/main/java/com/example/xworkout/ui/screens/perfil.kt
@@ -1,13 +1,37 @@
 package com.example.xworkout.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.xworkout.R
 
 @Preview(showBackground = true)
 @Composable
@@ -15,7 +39,134 @@ fun perfilView(){
     Box(
         modifier = Modifier.fillMaxSize()
     ){
-        Text(text = "perfil", modifier = Modifier.padding(16.dp))
+        LazyColumn (Modifier
+            .fillMaxSize()
+            .background(Color.DarkGray)) {
+            item {
+                Row(verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                        .border(2.dp, Color.Black,shape = RoundedCornerShape(10.dp))
+                        .padding(10.dp)) {
 
+                    Image(painter = painterResource(R.drawable.logo_x_workout),
+                        contentDescription = null, modifier = Modifier
+                            .size(120.dp)
+                            .clip(CircleShape)
+                            .border(
+                                2.dp, Color.Red,
+                                shape = CircleShape
+                            ))
+
+                    Column(horizontalAlignment = Alignment.CenterHorizontally, // Centra el contenido
+                        verticalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .padding(start = 20.dp)
+                            .wrapContentHeight()) {
+
+                        Text(text = "X-WORKOUTPerfil", color = Color.Red,
+                            fontWeight = androidx.compose.ui.text.font.FontWeight.Bold)
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        Text(text = "Nivel: DEFAULT")
+
+                        Spacer(modifier = Modifier.height(10.dp))
+
+                        Button(
+                            onClick = {},
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color.Transparent,
+                                contentColor = Color.White
+                            ),
+
+                            modifier = Modifier
+                                .padding(top = 8.dp)
+                                .border(2.dp, Color.Red, shape = RoundedCornerShape(12.dp)),
+                            shape = RoundedCornerShape(12.dp)
+                        ) {
+                            Text(
+                                text = "Editar Perfil",
+                                fontSize = 14.sp,
+                                modifier = Modifier.padding(horizontal = 8.dp)
+                            )
+                        }
+                    }
+
+                }
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Column (
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                        .border(2.dp, Color.Black,shape = RoundedCornerShape(10.dp))
+                        .padding(10.dp)) {
+
+                    Text(text = "SETTINGS", color = Color.White)
+
+                    FilaSettings("Barra de dominadas",false,{})
+                    FilaSettings("Anillas de gimnasia",false,{})
+                    FilaSettings("paralelas altas",false,{})
+                    FilaSettings("paralelas bajas",false,{})
+                    FilaSettings("Barra de dominadas",false,{})
+
+                }
+
+                Spacer(modifier = Modifier.height(10.dp))
+
+                Column (verticalArrangement = Arrangement.SpaceEvenly,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(20.dp)
+                        .border(2.dp, Color.Black,shape = RoundedCornerShape(10.dp))
+                        .height(150.dp)
+                        .padding(10.dp)) {
+
+                    Text(text = "Estadisticas", color = Color.White, fontSize = 20.sp)
+
+                    Row(modifier = Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.SpaceBetween) {
+                        Text(text = "TOTAL DE ENTRENAMIENTOS:", color = Color.White)
+                        Text(text = "0", color = Color.White)
+                    }
+
+                    Row(modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceBetween) {
+                        Text(text = "TOTAL DE Volumen:", color = Color.White)
+                        Text(text = "0", color = Color.White)
+                    }
+                }
+            }
+
+        }
+
+    }
+}
+
+@Composable
+fun FilaSettings(
+    label: String,
+    isChecked: Boolean,
+    onCheckedChange: (Boolean) -> Unit
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = label, color = Color.White, fontSize = 16.sp)
+        Switch(
+            checked = isChecked,
+            onCheckedChange = onCheckedChange,
+            colors = SwitchDefaults.colors(
+                checkedTrackColor = Color.Red,
+                checkedThumbColor = Color(0xFF1C1C1C)
+            )
+        )
     }
 }

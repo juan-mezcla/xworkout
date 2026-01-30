c8e7b181754bf908432cf25e37ca5bdb15fd0649
alumno alumno@DAM206 <juanmezcua96@gmail.com>
2026-01-30 14:16:55 +0100

    comenzando vista rutina generada

--- app/src/main/java/com/example/xworkout/ui/screens/generadorRutina.kt
+++ app/src/main/java/com/example/xworkout/ui/screens/generadorRutina.kt
@@ -1,13 +1,22 @@
 package com.example.xworkout.ui.screens
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.xworkout.R
 
 
 @Composable
@@ -17,7 +26,43 @@ fun generadorRutina(tipo: String) {
             modifier = Modifier.fillMaxSize()
         ){
             Text(text = "$tipo aa", modifier = Modifier.padding(16.dp))
+            LazyColumn () {
 
+                item {
+                    Column() {
+                        Row() {
+                            Image(painter = painterResource(R.drawable.ic_history),contentDescription = null)
+
+                            Column() {
+                                Row() {
+                                    Text(text = "titulo ejercicio")
+                                    Button(onClick = {}) {
+                                        Image(painter = painterResource(R.drawable.ic_history),contentDescription = null)
+                                    }
+                                }
+
+                                Text(text = "titulo ejercicio")
+
+                                Row() {
+                                    TextField(value = "actual reps")
+
+                                    Button(onClick = {}) {
+                                        Text(text = "Terminado")
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                }
+
+            }
         }
+
+}
+
+@Preview
+@Composable
+fun rutinaGenerada(){
 
 }
